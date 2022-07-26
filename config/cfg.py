b26be1020f81b6219ae288169e9143722f1d331d
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2022-07-26 15:37:02 +0700

    facial beauty prediction

---
+++ config/cfg.py
@@ -0,0 +1,12 @@
+from collections import OrderedDict
+
+cfg = OrderedDict()
+cfg['use_gpu'] = False
+cfg['scut_fbp_dir'] = '/home/xulu/DataSet/Face/SCUT-FBP/Crop'
+cfg['hotornot_dir'] = '/home/xulu/DataSet/Face/eccv2010_beauty_data/hotornot_face'
+cfg['cv_index'] = 1
+cfg['scutfbp5500_base'] = '/home/manhduy/Documents/dataset/SCUT-FBP5500_v2.1/SCUT-FBP5500_v2'
+
+cfg['batch_size'] = 4
+cfg['epoch'] = 2
+cfg['random_seed'] = 40
